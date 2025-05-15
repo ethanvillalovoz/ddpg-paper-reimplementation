6aa67217ead31037896fe729cec6127c4c67efa5
Ethan Villalovoz <ethan.villalovoz@gmail.com>
2025-05-15 00:30:52 -0700

    Updated DDPG.py with ActorNetwork class

--- DDPG.py
+++ DDPG.py
@@ -12,6 +12,7 @@
 import os
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.initializers import RandomUniform
 
 class OrnsteinUhlenbeckActionNoise:
     def __init__(self, mu, sigma, theta=0.2, dt=1e-2, x0=None):
@@ -34,23 +35,90 @@ class OrnsteinUhlenbeckActionNoise:
         self.x_prev = x
         return x
     
-    class ReplayBuffer(object):
-        def __init__(self, max_size, input_shape, n_actions):
-            self.mem_size = max_size
-            self.mem_cntr = 0
-            self.state_memory = np.zeros((self.mem_size, *input_shape), dtype=np.float32)
-            self.new_state_memory = np.zeros((self.mem_size, *input_shape), dtype=np.float32)
-            self.action_memory = np.zeros((self.mem_size, n_actions), dtype=np.float32)
-            self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
-            self.terminal_memory = np.zeros(self.mem_size, dtype=np.float32)
+class ReplayBuffer(object):
+    def __init__(self, max_size, input_shape, n_actions):
+        self.mem_size = max_size
+        self.mem_cntr = 0
+        self.state_memory = np.zeros((self.mem_size, *input_shape), dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_shape), dtype=np.float32)
+        self.action_memory = np.zeros((self.mem_size, n_actions), dtype=np.float32)
+        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.float32)
 
-        def store_transition(self, state, action, reward, state_, done):
-            index = self.mem_cntr % self.mem_size
-            self.state_memory[index] = state
-            self.new_state_memory[index] = state_
-            self.action_memory[index] = action
-            self.reward_memory[index] = reward
-            self.terminal_memory[index] = 1 - int(done)
-            self.mem_cntr += 1
+    def store_transition(self, state, action, reward, state_, done):
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = 1 - int(done)
+        self.mem_cntr += 1
 
-        
+    def sample_buffer(self, batch_size):
+        max_mem = min(self.mem_cntr, self.mem_size)
+        batch = np.random.choice(max_mem, batch_size, replace=False)
+        states = self.state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        new_states = self.new_state_memory[batch]
+        terminal = self.terminal_memory[batch]
+
+        return states, actions, rewards, new_states, terminal
+
+class ActorNetwork(object):
+    def __init__(self, lr, n_actions, input_dims, name, sess, fcl_dims, fc2_dims, action_bound, batch_size=64, chkpt_dir='tmp/ddpg'):
+        self.lr = lr
+        self.n_actions = n_actions
+        self.names = name
+        self.input_dims = input_dims
+        self.fcl_dims = fcl_dims
+        self.fc2_dims = fc2_dims
+        self.action_bound = action_bound
+        self.batch_size = batch_size
+        self.sess = sess
+        self.checkpoint_dir = chkpt_dir
+        self.build_network()
+        self.params = tf.trainable_variables(scope=self.name)
+        self.saver = tf.train.Saver()
+        self.checpoint_file = os.path.join(chkpt_dir, name + '_ddpg.ckpt')
+
+        self.unnormalized_actor_gradients = tf.gradients(self.mu, self.params, -self.action_gradient)
+
+        self.actor_gradients = list(map(lambda x: tf.div(x, self.batch_size), self.unnormalized_actor_gradients))
+        self.optimize = tf.train.AdamOptimizer(self.lr).apply_gradients(zip(self.actor_gradients, self.params))
+
+    def build_network(self):
+        with tf.variable_scope(self.name):
+            self.input = tf.placeholder(tf.float32, [None, *self.input_dims], name='input')
+            self.action_gradient = tf.placeholder(tf.float32, [None, self.n_actions], name='action_gradient')
+
+            f1 = 1 / np.sqrt(self.fcl_dims)
+            dense1 = tf.layers.dense(self.input, units=self.fc1dims, kernal_initializer=random_uniform(-f1, f1), bias_initializer=random_uniform(-f1, f1), name='dense1')
+
+            batch1 = tf.layers.batch_normalization(dense1)
+            activation1 = tf.nn.relu(batch1)
+
+            f2 = 1 / np.sqrt(self.fc2_dims)
+            dense2 = tf.layers.dense(activation1, units=self.fc2_dims, kernal_initializer=random_uniform(-f2, f2), bias_initializer=random_uniform(-f2, f2), name='dense2')
+            batch2 = tf.layers.batch_normalization(dense2)
+            activation2 = tf.nn.relu(batch2)
+
+            f3 = 0.003
+
+            mu = tf.layers.dense(activation2, units=self.n_actions, activation='tanh', kernal_initializer=random_uniform(-f3, f3), bias_initializer=random_uniform(-f3, f3), name='mu')
+
+            self.mu = tf.multiply(mu, self.action_bound, name='scaled_mu')
+
+    def predict(self, inputs):
+        return self.sess.run(self.mu, feed_dict={self.input: inputs})
+    
+    def train(self, inputs, gradients):
+        return self.sess.run(self.optimize, feed_dict={self.input: inputs, self.action_gradient: gradients})
+    def save_checkpoint(self):
+        print('... saving checkpoint ...')
+        self.saver.save(self.sess, self.checkpoint_file)
+
+    def load_checkpoint(self):
+        print('... loading checkpoint ...')
+        self.saver.restore(self.sess, self.checkpoint_file)
+
