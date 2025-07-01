fa08574906a288813f9df2b1b6f72d61cf67754d
ethanvillalovoz <ethan.villalovoz@gmail.com>
2025-06-30 18:41:43 -0700

    Implemented ReplayBuffer class

--- src/main.py
+++ src/main.py
@@ -60,10 +60,31 @@ class OUActionNoise(object):
 
 class ReplayBuffer(object):
     def __init__(self, max_size, input_shape, n_actions):
-        pass
+        self.memory_size = max_size
+        self.memory_counter = 0
+        self.state_memory = np.zeros((self.memory_size, *input_shape), dtype=np.float32)
+        self.new_state_memory = np.zeros((self.memory_size, *input_shape), dtype=np.float32)
+        self.action_memory = np.zeros((self.memory_size, n_actions), dtype=np.float32)
+        self.reward_memory = np.zeros(self.memory_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(self.memory_size, dtype=npfloat32)
 
     def store_transition(self, state, action, reward, new_state, done):
-        pass
+        index = self.memory_counter % self.memory_size
+        self.state_memory[index] = state
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.new_state_memory[index] = new_state
+        self.terminal_memory[index] =  1 - int(done)
+        self.memory_counter += 1
 
     def sample_buffer(self, batch_size):
-        pass
+        max_mem = min(self.memory_counter, self.memory_size)
+        batch = np.random.choice(max_mem, batch_size)
+
+        states = self.state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        new_states = self.new_state_memory[batch]
+        terminal = self.terminal_memory[batch]
+
+        return states, actions, rewards, new_states, terminal  
